8540486e8dca15078ed4b310b9c977aca7267c0f
Shun Furukawa <furukawa9312@gmail.com>
2021-01-06 14:57:13 +0900

    update terraform version

--- variables.tf
+++ variables.tf
@@ -1,6 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 2.0"
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
   profile = "furukawa-aws-cli"
 }
 
@@ -15,14 +23,14 @@ terraform {
 
 variable "images" {
   default = {
-    us-east-1 = "ami-1ecae776"
-    us-west-2 = "ami-e7527ed7"
-    us-west-1 = "ami-d114f295"
-    eu-west-1 = "ami-a10897d6"
-    eu-central-1 = "ami-a8221fb5"
+    us-east-1      = "ami-1ecae776"
+    us-west-2      = "ami-e7527ed7"
+    us-west-1      = "ami-d114f295"
+    eu-west-1      = "ami-a10897d6"
+    eu-central-1   = "ami-a8221fb5"
     ap-southeast-1 = "ami-68d8e93a"
     ap-southeast-2 = "ami-fd9cecc7"
     ap-northeast-1 = "ami-cbf90ecb"
-    sa-east-1 = "ami-b52890a8"
+    sa-east-1      = "ami-b52890a8"
   }
 }
